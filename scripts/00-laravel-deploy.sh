2692683b18a5cb13a039a209427f82ab00f30f5a
qoriib <nashrullah.122140162@student.itera.ac.id>
2026-02-21 14:06:46 +0700

    refactor: streamline Dockerfile and deployment script; remove unnecessary environment variable assignments and optimize composer command

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
-set -e
-
-echo "Running composer..."
-composer install --no-dev --no-interaction --optimize-autoloader --working-dir=/var/www/html
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
-php /var/www/html/artisan config:cache
+php artisan config:cache
 
 echo "Caching routes..."
-php /var/www/html/artisan route:cache
-
-echo "Running migrations..."
-php /var/www/html/artisan migrate --force
-
-echo "Starting PHP-FPM..."
-php-fpm &
-
-echo "Starting Nginx..."
-exec nginx -g 'daemon off;'
+php artisan route:cache
